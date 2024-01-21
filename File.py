77e78c199d2288f9703797ae87c026330970eaaa
arnav ar <arnavarora751@gmail.com>
2024-01-21 00:14:10 -0600

    initial commit

---
+++ File.py
@@ -0,0 +1,37 @@
+stay = True
+is_action_first = "First"
+action = input("First Action:")
+
+def handle(action = action, stay = stay):
+    match action:
+        case ":q":
+            stay = False
+        case ":c":
+            f = open("result.txt", "w")
+            f.write("")
+            f.close()
+        case ":r":
+            try:
+                f = open("result.txt", "r")
+                print(f.read())   
+            except:
+                print("No File Was Made")
+            
+        case _:
+            f = open("result.txt", "a")
+            f.write(action)
+            f.close()
+    return stay
+
+
+
+handle()
+stay = handle()
+while(stay):
+    action = input("Next Action:")
+    stay = handle(action)
+
+
+    
+
+
